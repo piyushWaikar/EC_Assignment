96dce7648ef89490aa99d31232d3e5165c8fffe6
Piyush W <hanumantw99@gmail.com>
2024-10-07 14:24:33 +0530

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,10 @@ function App() {
       element:<Layout/>,
       children:[
         {
+          path:'/',
+          element:<LoginPage/>
+        },
+        {
           path:'/login',
           element:<LoginPage/>
         },
